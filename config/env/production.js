a36aa723f0827fc2007acbefc04782c49184f4ca
willchertoff <willchertoff@gmail.com>
2016-11-03 10:11:14 -0700

    chore(build): Change production port

--- config/env/production.js
+++ config/env/production.js
@@ -2,5 +2,5 @@ export default {
   env: 'production',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
   db: 'mongodb://goat:goat-hcpd@ds053439.mlab.com:53439/hcpd',
-  port: 53439
+  port: 3000
 };
