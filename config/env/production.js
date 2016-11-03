a31da37662fc4c8d8e706e27a1608feb0bbf52c9
willchertoff <willchertoff@gmail.com>
2016-11-03 09:57:22 -0700

    chore(build): Fix errors in production db address

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 export default {
   env: 'production',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: 'mongodb://goat:goat-dev@ds053439.mlab.com:53439/hcpd'
+  db: 'mongodb://goat:goat-hcpd@ds053439.mlab.com:53439/hcpd',
   port: 53439
 };
