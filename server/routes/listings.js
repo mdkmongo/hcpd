b1d8333c39d67eda0b60dd2b6c5250d0dcbaa34a
willchertoff <willchertoff@gmail.com>
2016-11-10 15:41:35 -0800

    feat(listings): make /listings a protected route

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -2,23 +2,25 @@ import express from 'express';
 import listingsCtrl from '../controllers/listing';
 import validate from 'express-validation';
 import paramValidation from '../../config/param-validation';
+import expressJwt from 'express-jwt';
+import config from '../../config/env';
 
 const router = express.Router();  // eslint-disable-line new-cap
 
 router.route('/')
-  /** GET /api/listings/:siteId - Get site listings */
-  .get(listingsCtrl.list)
+  /** GET /api/listings/:siteId - Get listings */
+  .get(expressJwt({ secret: config.jwtSecret }), listingsCtrl.list)
 
-  .post(validate(paramValidation.createListing), listingsCtrl.create);
+  .post(expressJwt({ secret: config.jwtSecret }), validate(paramValidation.createListing), listingsCtrl.create); // eslint-disable-line max-len
 
 router.route('/:listingId')
 
   .get(listingsCtrl.get)
 
-  /** PUT /api/sites/:listingId - Update site */
-  .put(validate(paramValidation.updateListing), listingsCtrl.update)
+  /** PUT /api/sites/:listingId - Update listing */
+  .put(expressJwt({ secret: config.jwtSecret }), validate(paramValidation.updateListing), listingsCtrl.update) // eslint-disable-line max-len
 
-  .delete(listingsCtrl.remove);
+  .delete(expressJwt({ secret: config.jwtSecret }), listingsCtrl.remove);
 
 /** Load listings when API with siteId route parameter is hit */
 router.param('listingId', listingsCtrl.load);
--- server/tests/listings.test.js
+++ server/tests/listings.test.js
@@ -20,10 +20,46 @@ describe('## Listing APIs', () => {
     state: 'Kansas'
   };
 
+  const user = {
+    username: 'Test User',
+    site_name: 'Test Site',
+    password: 'lakings',
+    is_admin: false,
+    mobile_number: '666-666-6666'
+  };
+
+  describe('# POST /api/users', () => {
+    it('should create a new user for listing auth testing', (done) => {
+      request(app)
+        .post('/api/users')
+        .send(user)
+        .expect(httpStatus.OK)
+        .then(res => {
+          expect(res.body.username).to.equal(user.username);
+          user._id = res.body._id;
+          done();
+        });
+    });
+  });
+  describe('# POST /api/login', () => {
+    it('should login a user when correct credentials are supplied', (done) => {
+      request(app)
+        .post('/api/auth/login')
+        .send(user)
+        .expect(httpStatus.OK)
+        .then(res => {
+          expect(res.body.username).to.equal(user.username);
+          expect(res.body).to.have.any.key('token');
+          user.token = res.body.token;
+          done();
+        });
+    });
+  });
   describe('# GET /api/listings/:siteId', () => {
     it('should get site listings', (done) => {
       request(app)
         .get(`/api/listings?siteId=${site.site_id}`)
+        .set('Authorization', `Bearer ${user.token}`)
         .expect(httpStatus.OK)
         .then(res => {
           expect(res.body).to.be.an('array');
@@ -35,6 +71,7 @@ describe('## Listing APIs', () => {
     it('should add a listing', (done) => {
       request(app)
         .post('/api/listings')
+        .set('Authorization', `Bearer ${user.token}`)
         .send(listing)
         .expect(httpStatus.OK)
         .then(res => {
@@ -48,6 +85,7 @@ describe('## Listing APIs', () => {
     it('should get a single listing', (done) => {
       request(app)
         .get(`/api/listings/${listing.id}`)
+        .set('Authorization', `Bearer ${user.token}`)
         .expect(httpStatus.OK)
         .then(res => {
           expect(res.body.first_name).to.equal(listing.firstName);
@@ -55,12 +93,12 @@ describe('## Listing APIs', () => {
         });
     });
   });
-
   describe('# PUT /api/listings/:listingId', () => {
     it('should update a single listing', (done) => {
       listing.firstName = 'UpdateTest';
       request(app)
         .put(`/api/listings/${listing.id}`)
+        .set('Authorization', `Bearer ${user.token}`)
         .send(listing)
         .expect(httpStatus.OK)
         .then(res => {
@@ -73,9 +111,21 @@ describe('## Listing APIs', () => {
     it('should delete a single listing', (done) => {
       request(app)
         .delete(`/api/listings/${listing.id}`)
+        .set('Authorization', `Bearer ${user.token}`)
         .expect(httpStatus.OK)
         .then(res => {
           expect(res.body._id).to.equal(listing.id);
+          done();
+        });
+    });
+  });
+  describe('# DELETE /api/users/', () => {
+    it('should delete user', (done) => {
+      request(app)
+        .delete(`/api/users/${user._id}`)
+        .expect(httpStatus.OK)
+        .then(res => {
+          expect(res.body.username).to.equal(user.username);
           done();
         });
     });
